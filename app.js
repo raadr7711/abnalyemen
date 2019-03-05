767e741b094782e032479fe617271cd44acdb7fe
raadr7711 <38633188+raadr7711@users.noreply.github.com>
2019-03-05 17:34:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var app = require('express')();
-var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var app = function () {
+  var http = require('http');
+  var express = require('express');
+  var app = express();
 
-app.use(express.static(__dirname + '/views'));
+  app.set('views', __dirname + '/views');
+  app.engine('html', require('ejs').renderFile);
+  app.get('/', function(req, res) {
+    res.render('index.html');
+  });
 
+  return app;
+}();
 
-app.get('/', function (req, res) {
-    res.sendfile('index.html');
-});
 module.exports = app;
