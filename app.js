2b77f0d1f566b63c57bee06f20b8457ffc00b0a7
raadr7711 <38633188+raadr7711@users.noreply.github.com>
2019-03-05 08:48:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var app = function () {
   var express = require('express');
   var app = express();
 
-  app.set('views', __dirname + '/views');
+  app.set('views', __dirname + '/');
   app.engine('html', require('ejs').renderFile);
-  app.get('/', function(req, res) {
+  app.get('/views', function(req, res) {
     res.render('index.html');
   });
 
