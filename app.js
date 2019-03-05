afb11ad5722c2d4c35b6009d78f5acdc301815c3
raadr7711 <38633188+raadr7711@users.noreply.github.com>
2019-03-05 09:04:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,12 +12,4 @@ var app = function () {
   return app;
 }();
 
-var fs = require('fs');
-
-http.createServer(function (req,res){
-    fs.readFile('index.html',function(err,data){
-    res.writeHead(200,{'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
 module.exports = app;
